615eaff7228928dbc63dbd58816c1659700a007b
scarfinor <scarfinor@gmail.com>
2024-07-19 22:06:34 -0500

    git commit MARS-ROVER-STARTER first look

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,12 +1,17 @@
-const Command = require('../command.js');
+const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Command class", function() {
+describe("Command class", function () {
+  let instantiateCommand = function () {
+    new Command();
+  };
 
-  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
-    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+  // Test 1
+  test("throws error if command type is NOT passed into constructor as the first parameter", function () {
+    expect(instantiateCommand).toThrow(new Error("Command type required."));
   });
 
+  // Test 2
 });
